0b9bcee1c21c71324e82245dd9df6fec228f1ca3
lnatamm <lnatamm25@gmail.com>
2024-09-14 10:23:51 -0300

    implementando bfs

--- Grafo/Grafo.java
+++ Grafo/Grafo.java
@@ -1,5 +1,8 @@
 package Grafo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Grafo {
 
     private int[][] matrix;
@@ -51,6 +54,43 @@ public class Grafo {
 
     public boolean isBinarySymetric(){
         return isBinary() && isSymetric();
+    }
+
+    public int[][] bfs(int r){
+
+        int n = matrix.length;
+        int[] colors = new int[n];
+        int[] distance = new int[n];
+        int[] predecessor = new int[n];
+        for(int i = 0; i < n; i++){
+            colors[i] = 0;
+            distance[i] = Integer.MAX_VALUE;
+            predecessor[i] = -1;
+        }
+        colors[r] = 1;
+        distance[r] = 0;
+        predecessor[r] = -1;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(r);
+
+        while(!queue.isEmpty()){
+            int u = queue.poll();
+            for(int v = 0; v < n; v++){
+                if(matrix[u][v] == 1 && colors[v] == 0){
+                    colors[v] = 1;
+                    predecessor[v] = u;
+                    distance[v] = distance[u] + 1;
+                    queue.add(v);
+                }
+            }
+            colors[u] = 2;
+        }
+        int[][] ans = new int[][]{
+                colors,
+                distance,
+                predecessor
+        };
+        return ans;
     }
 
 }
