5d52076bc012f07f62a1b6e1a8d6ca59f694f2e9
lnatamm <lnatamm25@gmail.com>
2024-09-14 11:22:01 -0300

    testando dfs

--- Grafo/Main.java
+++ Grafo/Main.java
@@ -3,11 +3,16 @@ package Grafo;
 public class Main {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
-                {0, 1, 0},
-                {1, 0, 1},
-                {0, 1, 0}
+                {0, 1, 1, 0, 0},
+                {1, 0, 1, 0, 0},
+                {1, 1, 0, 1, 1},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0}
         };
         Grafo g = new Grafo(matrix);
-        g.printMatrix();
+        int[] ans = g.dfs();
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
     }
 }
